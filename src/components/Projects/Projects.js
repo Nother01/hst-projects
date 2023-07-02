a36bc68a257d76c51add2100c0abd3a9f70be0b6
Nother01 <hugo.sapey-triomphe@epitech.eu>
2023-07-02 21:58:48 +0200

    change: insert LampeTorche /projets

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,9 +2,10 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
 import bitsOfCode from "../../Assets/Projects/blog.png";
+
+import assetLampeTorche from "../../Assets/Projects/assetLampeTorche.png";
+
 
 function Projects() {
   return (
@@ -12,18 +13,30 @@ function Projects() {
       <Particle />
       <Container>
         <h1 className="project-heading">
-          Mes récents <strong className="purple">Projets </strong>
+          Projet <strong className="purple"> open source</strong>
         </h1>
         <p style={{ color: "white" }}>
-          Voici quelque projet sur lequel j'ai travaillé.
+          Voici quelque projet personnel.
+          <br />
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={assetLampeTorche}
+              isBlog={false}
+              title="LampeTorche2"
+              description="Ce projet permet de télécharger des titres d'une playlist Spotify depuis YouTube au format MP3. Conçu en JS avec l'API Spotify et YouTube."
+              ghLink="https://github.com/Nother01/LampeTorche2"
+              demoLink="https://google.com"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={bitsOfCode}
               isBlog={false}
               title="Coming Soon"
-              description="Cum imaginem suos Eubuli post laniatu calcibus commeatuum addictum lacrimosum discerpsit calcibus sui furoris discerpsit sibi imperiali quod miserando famis famis cum pugnis clari cuiusdam quod quisque inter exitio suos furoris iudicio quod furoris furoris furoris."
+              description="Litpon haec peregrinos oriens in egressus caeli oriens ter limitibus egressus Vadomarium et oriens et egressus Valentiam in perferret petit."
               ghLink="https://google.com"
               demoLink="https://google.com"
             />
@@ -34,7 +47,7 @@ function Projects() {
               imgPath={bitsOfCode}
               isBlog={false}
               title="Coming Soon"
-              description="Tepore confines perferret tepore arma Caesaris moturus reges in vastabantur quorum vastabantur septies confines arma terrae Gundomadum vastabantur arma oriens in in egressus caeli ter oriens ter limitibus Vadomarium et oriens et egressus Valentiam in perferret petit."
+              description="Litpon haec peregrinos oriens in egressus caeli oriens ter limitibus egressus Vadomarium et oriens et egressus Valentiam in perferret petit."
               ghLink="https://google.com"
               demoLink="https://google.com"
             />
@@ -42,10 +55,21 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={bitsOfCode}
               isBlog={false}
               title="Coming Soon"
-              description="Tabernas similis et similis harum alias digressurus si lecturos ea monstranda ad haec peregrinos propria alias posse existimo nusquam gererentur Romae mirari existimo peregrinos perstringam gererentur alias haec lecturos si causas et propria si et vilitates oratio lecturos causas gererentur."
+              description="Litpon haec peregrinos oriens in egressus caeli oriens ter limitibus egressus Vadomarium et oriens et egressus Valentiam in perferret petit."
+              ghLink="https://google.com"
+              demoLink="https://google.com"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={bitsOfCode}
+              isBlog={false}
+              title="Coming Soon"
+              description="Litpon haec peregrinos oriens in egressus caeli oriens ter limitibus egressus Vadomarium et oriens et egressus Valentiam in perferret petit."
               ghLink="https://google.com"
               demoLink="https://google.com"
             />
